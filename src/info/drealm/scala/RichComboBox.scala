e214c06a5dd131e7763c6e8cbe98c5e0120648bd
Peter L Jones <peter@drealm.info>
2014-05-11 22:34:47 +0100

    Enable focus on editable combobox to be tracked

--- src/info/drealm/scala/RichComboBox.scala
+++ src/info/drealm/scala/RichComboBox.scala
@@ -1,8 +1,9 @@
 package info.drealm.scala
 
 import scala.swing.ComboBox
+import scala.swing.event._
 
-class RichComboBox[A](items: Seq[A], private[this] val _name: String, private[this] val _label: scala.swing.Label) extends ComboBox[A](items) {
+class RichComboBox[A](items: Seq[A], _name: String, _label: scala.swing.Label) extends ComboBox[A](items) {
     def this(items: Seq[A], _name: String) = this(items, _name, null)
     def this(items: Seq[A]) = this(items, "", null)
 
@@ -10,5 +11,11 @@ class RichComboBox[A](items: Seq[A], private[this] val _name: String, private[th
     if (_label != null) {
         // Uhhhhh, right...
         _label.peer.setLabelFor(peer.asInstanceOf[java.awt.Component])
+    }
+
+    override def makeEditable()(implicit editor: ComboBox[A] => ComboBox.Editor[A]) {
+        super.makeEditable()
+        listenTo(editor(this).component)
+        reactions += { case e: FocusGained => publish(new eventX.CbxEditorFocused(this)) }
     }
 }

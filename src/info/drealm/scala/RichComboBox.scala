a19b333e430c1cd7f62be1884390aca295ca294b
Peter L Jones <peter@drealm.info>
2014-05-18 19:00:13 +0100

    Minor tweaks to make RichComboBox aesthetically nicer

--- src/info/drealm/scala/RichComboBox.scala
+++ src/info/drealm/scala/RichComboBox.scala
@@ -1,16 +1,16 @@
 package info.drealm.scala
 
-import scala.swing.ComboBox
+import scala.swing.{ComboBox, Label}
 import scala.swing.event._
 
-class RichComboBox[A](items: Seq[A], _name: String, _label: scala.swing.Label) extends ComboBox[A](items) {
+class RichComboBox[A](items: Seq[A], _name: String, _label: Label) extends ComboBox[A](items) {
     def this(items: Seq[A], _name: String) = this(items, _name, null)
     def this(items: Seq[A]) = this(items, "", null)
 
     name = _name
     if (_label != null) {
         // Uhhhhh, right...
-        _label.peer.setLabelFor(peer.asInstanceOf[java.awt.Component])
+        _label.peer.setLabelFor(peer.asInstanceOf[javax.swing.JComboBox[_]])
     }
 
     override def makeEditable()(implicit editor: ComboBox[A] => ComboBox.Editor[A]) {

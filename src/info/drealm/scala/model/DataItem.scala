a8c0105db87e7d0acda24ddf8a5acb67d8a0fc79
Peter L Jones <peter@drealm.info>
2014-06-05 19:08:57 +0100

    Review of code against C# TrapKATEditor

--- src/info/drealm/scala/model/DataItem.scala
+++ src/info/drealm/scala/model/DataItem.scala
@@ -28,10 +28,37 @@ import java.io._
 import scala.io._
 
 trait DataItem extends scala.swing.Publisher {
-    private[this] var _dataItemChanged: Boolean = false
-    def hasChanged = _dataItemChanged
-    def dataItemChanged = if (!_dataItemChanged) {
-        _dataItemChanged = true
+    // The C# has this as protected
+    final def deserialize(in: FileInputStream): Unit = deserialize(new DataInputStream(in))
+    def deserialize(in: DataInputStream): Unit
+
+    // The C# has this as public
+    final def serialize(out: FileOutputStream): Unit = serialize(out, false)
+    final def serialize(out: DataOutputStream): Unit = serialize(out, false)
+
+    // The C# has this as protected
+    final def serialize(out: FileOutputStream, saving: Boolean): Unit = serialize(new DataOutputStream(out), saving)
+    // And this is the one you MUST define
+    // ALWAYS ALWAYS! ALWAYS!!! call save(out) if saving is true when
+    // implementing this!!!!!
+    def serialize(out: DataOutputStream, saving: Boolean): Unit
+
+    // The C# has "internalchg" as protected but not used, so not implemented!
+
+    // The C# has this as private
+    // _changed tracks whether update has been called on this DataItem
+    private[this] var _changed: Boolean = false
+
+    // The C# is a little different here
+    // It has "OnDataChanged" which the subclasses call when they've done
+    // their own updating - no update(u) call.  There's also no check
+    // on _changed before notifying listeners.  I think this implementation
+    // is a little nicer, both for subclasses and listeners.
+    // (Don't use update(u) if you want to only send the DataItemChanged
+    // after a series of changes - do them (all the "u" functions) and then call
+    // dataItemChanged directly.)
+    protected def dataItemChanged = if (!_changed) {
+        _changed = true
         publish(new info.drealm.scala.eventX.DataItemChanged(this))
     }
     protected def update(u: => Unit) = {
@@ -39,22 +66,13 @@ trait DataItem extends scala.swing.Publisher {
         dataItemChanged
     }
 
-    final def deserialize(in: FileInputStream): Unit = deserialize(new DataInputStream(in))
-    def deserialize(in: DataInputStream): Unit
-    
+    // The C# has this as public
     final def save(out: FileOutputStream): Unit = save(new DataOutputStream(out))
     // In strange circumstances you may want to override this
     def save(out: DataOutputStream): Unit = {
         serialize(out, true)
-        _dataItemChanged = false
+        _changed = false
     }
-
-    final def serialize(out: FileOutputStream): Unit = serialize(out, false)
-    final def serialize(out: FileOutputStream, saving: Boolean): Unit = serialize(new DataOutputStream(out), saving)
-    final def serialize(out: DataOutputStream): Unit = serialize(out, false)
-    
-    // And this is the one you MUST define
-    // ALWAYS ALWAYS! ALWAYS!!! call save(out) if saving is true when
-    // implementing this!!!!!
-    def serialize(out: DataOutputStream, saving: Boolean): Unit
+    // The C# has this as public
+    def changed = _changed
 }

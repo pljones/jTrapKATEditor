b9305f7be779f9ff2df588c8f001348dc94208b2
Peter L Jones <peter@drealm.info>
2014-06-23 20:09:04 +0100

    Support for localization

---
+++ src/info/drealm/scala/Localization.scala
@@ -0,0 +1,44 @@
+/**
+ * **************************************************************************
+ *                                                                          *
+ *   (C) Copyright 2014 by Peter L Jones                                    *
+ *   pljones@users.sf.net                                                   *
+ *                                                                          *
+ *   This file is part of jTrapKATEditor.                                   *
+ *                                                                          *
+ *   jTrapKATEditor is free software; you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published by   *
+ *   the Free Software Foundation; either version 3 of the License, or      *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   jTrapKATEditor is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU General Public License for more details.                           *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with jTrapKATEditor.  If not, see http://www.gnu.org/licenses/   *
+ *                                                                          *
+ * **************************************************************************
+ */
+
+package info.drealm.scala
+
+import java.util.{ Locale, ResourceBundle }
+
+object Localization {
+    private[this] val stringBundle = ResourceBundle.getBundle("info.drealm.scala.Localization", Locale.getDefault())
+    def G(lookup: String): String = try {
+        stringBundle.getString(lookup)
+    }
+    catch {
+        case e: java.util.MissingResourceException => "<<" + lookup + ">>"
+    }
+    def G(lookup: String, args: String*): String = {
+        def from(n: Int): Stream[Int] = n #:: from(n + 1)
+
+        var result = G(lookup)
+        args zip from(0) foreach (x => result = result.replaceAll(s"[{]${x._2}[}]", s"${x._1}"))
+        result
+    }
+}

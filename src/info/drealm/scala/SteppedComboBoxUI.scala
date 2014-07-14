24cd059312bf46a9da7ffce023cbbdbe3ba3df11
Peter L Jones <peter@drealm.info>
2014-07-14 13:47:48 +0100

    Refactor SteppedComboBoxUI and capture an idea about extending JComboBox

--- src/info/drealm/scala/SteppedComboBoxUI.scala
+++ src/info/drealm/scala/SteppedComboBoxUI.scala
@@ -27,37 +27,49 @@ import java.awt._
 import javax.swing._
 import javax.swing.plaf.basic._
 
-// For some reason I do not yet understand, this loses platform look'n'feel
-class SteppedComboBoxUI(cbx: RichComboBox[_]) extends BasicComboBoxUI {
-    val peer = cbx.peer.asInstanceOf[JComboBox[_]]
-
-    override def createPopup: ComboPopup = {
-        new BasicComboPopup(peer) {
-
-            override def show: Unit = {
-                lazy val width: Int = {
-                    val metrics: FontMetrics = peer.getFontMetrics(peer.getFont());
-                    ((0 to (peer.getItemCount() - 1)) map (i => peer.getItemAt(i))).foldLeft(peer.getBounds().width)((maxWidth, item) => {
-                        val width = metrics.stringWidth(item.toString())
-                        if (width > maxWidth) width else maxWidth
-                    })
-                }
-                lazy val popupBounds = computePopupBounds(0, peer.getBounds().height, if (width == 0) peer.size.width else width, getPopupHeightForRowCount(peer.getMaximumRowCount()))
-                scroller.setMaximumSize(popupBounds.getSize());
-                scroller.setPreferredSize(popupBounds.getSize());
-                scroller.setMinimumSize(popupBounds.getSize());
-                list.invalidate()
-                peer.getSelectedIndex() match {
-                    case -1 => list.clearSelection()
-                    case i  => list.setSelectedIndex(i)
-                }
-                list.ensureIndexIsVisible(list.getSelectedIndex())
-                setLightWeightPopupEnabled(peer.isLightWeightPopupEnabled())
-                this.show(peer, popupBounds.x, popupBounds.y)
-            }
-
-            getAccessibleContext().setAccessibleParent(peer)
+// Because this extends BasicComboBoxUI, we forfeit platform look and feel
+class SteppedComboBoxUI(peer: JComboBox[_]) extends BasicComboBoxUI {
+    override def createPopup: ComboPopup = new BasicComboPopup(peer) {
+        lazy val myWidth: Int = {
+            val metrics: FontMetrics = peer.getFontMetrics(peer.getFont());
+            ((0 to (peer.getItemCount() - 1)) map (i => peer.getItemAt(i))).foldLeft(peer.getBounds().width)((maxWidth, item) => {
+                val width = metrics.stringWidth(item.toString())
+                if (width > maxWidth) width else maxWidth
+            }) + arrowButton.getBounds().width + 2
         }
-
+        lazy val popupBounds = computePopupBounds(0, peer.getBounds().height, myWidth, getPopupHeightForRowCount(peer.getMaximumRowCount()))
+        override def show: Unit = {
+            scroller.setMaximumSize(popupBounds.getSize());
+            scroller.setPreferredSize(popupBounds.getSize());
+            scroller.setMinimumSize(popupBounds.getSize());
+            list.invalidate()
+            peer.getSelectedIndex() match {
+                case -1 => list.clearSelection()
+                case i  => list.setSelectedIndex(i)
+            }
+            list.ensureIndexIsVisible(list.getSelectedIndex())
+            setLightWeightPopupEnabled(peer.isLightWeightPopupEnabled())
+            this.show(peer, popupBounds.x, popupBounds.y)
+        }
+        getAccessibleContext().setAccessibleParent(peer)
     }
+}
+
+// However, for some reason, I cannot get scala.swing.ComboBox to take this as a peer
+class SteppedJComboBox[A](model: ComboBoxModel[A]) extends JComboBox[A](model) {
+    private[this] var layingOut = false
+    private[this] lazy val width = {
+        val metrics: java.awt.FontMetrics = getFontMetrics(getFont());
+        ((0 to (getItemCount() - 1)) map (i => getItemAt(i))).foldLeft(getBounds().width)((maxWidth, item) => {
+            val width = metrics.stringWidth(item.toString())
+            if (width > maxWidth) width else maxWidth
+        }) + 3
+    }
+    override def doLayout = {
+        layingOut = true
+        try { super.doLayout() } finally { layingOut = false }
+    }
+
+    override def getSize: java.awt.Dimension = new java.awt.Dimension(
+        if (!layingOut) width else super.getWidth(), super.getHeight())
 }

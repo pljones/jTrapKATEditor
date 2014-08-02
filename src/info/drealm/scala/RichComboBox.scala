e4ad69da11f0235f89bd8d1c30299302b7deeb37
Peter L Jones <peter@drealm.info>
2014-08-02 18:26:07 +0100

    Cannot cast a wrapped JTextField to a Scala swing.TextField.  Sadly.

--- src/info/drealm/scala/RichComboBox.scala
+++ src/info/drealm/scala/RichComboBox.scala
@@ -80,11 +80,6 @@ class RichComboBox[A](items: Seq[A], _name: String, _label: Label) extends Combo
         }
     ))
 
-    def editor: Option[swing.TextField] = editable match {
-        case true  => Some(swing.Component.wrap(editorPeer).asInstanceOf[swing.TextField])
-        case false => None
-    }
-
     def editorPeer: javax.swing.JTextField = editable match {
         case true  => peer.getEditor().getEditorComponent().asInstanceOf[javax.swing.JTextField]
         case false => null

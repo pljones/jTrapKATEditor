896b38f8644e0f44bb40599604c148c18afb821a
Peter L Jones <peter@drealm.info>
2014-08-24 11:50:20 +0100

    Fix naming RichComboBox - name is a function not a value

--- src/info/drealm/scala/RichComboBox.scala
+++ src/info/drealm/scala/RichComboBox.scala
@@ -28,12 +28,14 @@ import javax.swing.JComboBox
 import scala.swing.{ ComboBox, Label, Swing }
 import scala.swing.event._
 
-class RichComboBox[A](items: Seq[A], override val name: String, _label: Label, stepped: Boolean = false) extends ComboBox[A](items) {
+class RichComboBox[A](items: Seq[A], _name: String, _label: Label, stepped: Boolean = false) extends ComboBox[A](items) {
     def this(items: Seq[A], _name: String, stepped: Boolean) = this(items, _name, null, stepped)
     def this(items: Seq[A], _name: String) = this(items, _name, null)
     def this(items: Seq[A], stepped: Boolean) = this(items, "", null, stepped)
     def this(items: Seq[A]) = this(items, "", null)
 
+    name = _name
+    
     if (stepped) {
         peer.setUI(SteppedComboBoxUI.getSteppedComboBoxUI(peer.asInstanceOf[JComboBox[_]]))
     }

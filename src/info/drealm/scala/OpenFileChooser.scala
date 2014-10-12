a54c70a3e3905619c683c62687613f9bdf5cd955
Peter L Jones <peter@drealm.info>
2014-10-12 13:52:00 +0100

    Add missing copyright notices

--- src/info/drealm/scala/OpenFileChooser.scala
+++ src/info/drealm/scala/OpenFileChooser.scala
@@ -1,3 +1,27 @@
+/**
+ * **************************************************************************
+ *                                                                          *
+ *   (C) Copyright 2014 by Peter L Jones                                    *
+ *   pljones@users.sf.net                                                   *
+ *                                                                          *
+ *   This file is part of jTrapKATEditor.                                   *
+ *                                                                          *
+ *   jTrapKATEditor is free software; you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published by   *
+ *   the Free Software Foundation; either version 3 of the License, or      *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   jTrapKATEditor is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU General Public License for more details.                           *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with jTrapKATEditor.  If not, see http://www.gnu.org/licenses/   *
+ *                                                                          *
+ * **************************************************************************
+ */
+
 package info.drealm.scala
 
 import java.io.File

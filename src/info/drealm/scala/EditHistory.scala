2e01a6576d4fc2e06cd40446584ed4b4b384ce9d
Peter L Jones <peter@drealm.info>
2014-11-01 22:02:12 +0000

    Add undo/redo storage

--- src/info/drealm/scala/EditHistory.scala
+++ src/info/drealm/scala/EditHistory.scala
@@ -1,6 +1,25 @@
 package info.drealm.scala
 
+trait HistoryAction { val actionName: String; def undoAction(): Unit; def redoAction(): Unit }
 object EditHistory {
-    def atStart = true
-    def nextItem = Nil
+    private[this] var _editHistory = List[HistoryAction]()
+    private[this] var _depth = 0
+
+    def canUndo: Boolean = _depth < _editHistory.length
+    def undoAction(): Unit = if (canUndo) _editHistory.drop(_depth).headOption map (x => { _depth = _depth + 1; x.undoAction() })
+    def undoActionName: Option[String] = if (canUndo) _editHistory.drop(_depth).headOption.map(x => x.actionName) else None
+
+    def canRedo: Boolean = _depth > 0
+    def redoAction(): Unit = if (canRedo) _editHistory.drop(_depth - 1).headOption map (x => { _depth = _depth - 1; x.redoAction() })
+    def redoActionName: Option[String] = if (canRedo) _editHistory.drop(_depth - 1).headOption.map(x => x.actionName) else None
+
+    def add(action: HistoryAction): Unit = {
+        _editHistory = action :: _editHistory.drop(_depth);
+        _depth = 0
+    }
+    
+    def clear(): Unit = {
+        _editHistory = List()
+        _depth = 0
+    }
 }
